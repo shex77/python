9555ec2b23e3dc858c48c30f2d9e174cb229793b
shex77 <69957344+shex77@users.noreply.github.com>
2020-12-27 22:13:08 +0300

    Update Shafo.py

--- Shafo.py
+++ Shafo.py
@@ -122,8 +122,8 @@ print  """\033[1;91m
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "SHAFO
-CorrectPassword = "SHAFO
+CorrectUsername =SHAFO
+CorrectPassword =Shafo
 
 loop = 'true'
 while (loop == 'true'):
