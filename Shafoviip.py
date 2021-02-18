a6a00b921c52450fbae6dc2bb0fe4ab6b3438d4d
shex77 <69957344+shex77@users.noreply.github.com>
2021-02-18 10:55:57 +0300

    Update Shafoviip.py

--- Shafoviip.py
+++ Shafoviip.py
@@ -1,53 +1,145 @@
-try:
-import uuid
-import time
-except Exception as e:
-print(e)
+import requests 
+
+    import uuid 
+
+    import time 
+
+except Exception as e: 
+
+    print(e) 
 
 logo = ("""
 
-("--------------------------")
+          ("--------------------------")
 
-("Instagram ib2_n)
+          ("Follow me instagram @sha_fo_ka")
 
-("--------------------------")
+          ("--------------------------")
+
+                  ("bot spam")
 
 """)
 
-print(logo)
+ 
 
-user = input(': ')
+print(logo) 
 
-password = input(': ')
+user = input instay xot(': ') 
 
-target = str(input((":")))
+password = input pass daxl ka(': ') 
 
-sle = int(input(": "))
+target = str(input(instay kabra(":"))) 
 
-def login():
+sle = int(input(": ")) 
 
-global target
+def login(): 
 
-r = requests.Session()
+    global target 
 
-uid = str(uuid.uuid4())
+    r = requests.Session() 
 
-url = "https://i.instagram.com/api/v1/accounts/login/"
+ 
 
-headers = {
+    uid = str(uuid.uuid4()) 
 
-'User-Agent': 'Instagram 113.0.0.39.122 Android (24/5.0; 515dpi; 1440x2416; huawei/google; Nexus 6P; angler; angler; en_US)',
+ 
 
-"Accept": "*/*",
+    url = "https://i.instagram.com/api/v1/accounts/login/" 
 
-"Accept-Encoding": "gzip, deflate",
+ 
 
-"Accept-Language": "en-US",
+    headers = { 
 
-"X-IG-Capabilities": "3brTvw==",
+        'User-Agent': 'Instagram 113.0.0.39.122 Android (24/5.0; 515dpi; 1440x2416; huawei/google; Nexus 6P; angler; angler; en_US)', 
 
-"X-IG-Connection-Type": "WIFI",
+        "Accept": "*/*", 
 
-"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Accept-Encoding": "gzip, deflate", 
 
-'Host': 'i.instagram.com'
+        "Accept-Language": "en-US", 
+
+        "X-IG-Capabilities": "3brTvw==", 
+
+        "X-IG-Connection-Type": "WIFI", 
+
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8", 
+
+        'Host': 'i.instagram.com' 
+
+    } 
+
+ 
+
+    data = { 
+
+        '_uuid': uid, 
+
+        'username': user, 
+
+        'enc_password': '#PWD_INSTAGRAM_BROWSER:0:1589682409:{}'.format(password), 
+
+        'queryParams': '{}', 
+
+        'optIntoOneTap': 'false', 
+
+        'device_id': uid, 
+
+        'from_reg': 'false', 
+
+        '_csrftoken': 'missing', 
+
+        'login_attempt_count': '0' 
+
+    } 
+
+ 
+
+    loginreq = r.post(url, data=data, headers=headers, allow_redirects=True) 
+
+    print(loginreq.text) 
+
+ 
+
+ 
+
+    if loginreq.text.find("is_private") >= 0: 
+
+        done = 0 
+
+        print("DONE LOGIN") 
+
+        r.headers.update({'X-CSRFToken': loginreq.cookies['csrftoken']}) 
+
+        url_id = "https://www.instagram.com/{}/?__a=1".format(target) 
+
+        url_get_user_id = r.get(url_id).json() 
+
+        print(url_get_user_id) 
+
+        while True: 
+
+            user_id = str(url_get_user_id["logging_page_id"]) 
+
+            your_user_id = str(user_id.split("_")[1])# 4231341234
+
+            urlRep = "https://i.instagram.com/users/" + your_user_id + "/report/" 
+
+            datas = {  
+
+                    'source_name': '', 'reason_id': 1, 'frx_context': ''  # spam
+
+                } 
+
+            req_SessionID = r.post(urlRep, data=datas) 
+
+            done += 1 
+
+            print(f"تم سبام  -> rs3wd : {done}") 
+
+            time.sleep(sle) 
+
+    else: 
+
+        print("LOGIN FAILED CHECK YOUR INFO!") 
+
+login()
